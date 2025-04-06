271aaf92503132eb06de4e5d872eec4c2cc969c9
SABRINA FRITZ <SABRINA_FRITZ@GMX.NET>
2025-04-06 11:42:21 +0200

    navbar erstellt

--- sakura/src/app/landingPage/landingPage.component.ts
+++ sakura/src/app/landingPage/landingPage.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
     selector: 'app-landingPage',
     standalone: true, 
-    imports: [],
+    imports: [NavbarComponent],
     template: `<section>
+                    <div>
+                        <app-navbar></app-navbar>
+                    </div>
                     <h1 class="fontRaleway">SAKURA RAMEN</h1>
                     <h2 class="fontRaleway">BEST RAMEN IN TOWN</h2>
                 </section>`,
